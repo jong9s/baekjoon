ede0e5f26d8e9691c57d61f84fcc644540297a9c
JongHun Seo <162901825+jonghun0915@users.noreply.github.com>
2024-10-28 15:22:47 +0900

    [Bronze III] Title: 최소, 최대, Time: 1700 ms, Memory: 318468 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		int N = scan.nextInt();
+		int[] arr = new int[N];
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = scan.nextInt();
+		}
+		int max = arr[0];
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] > max) {
+				max = arr[i];
+			}
+		}
+		int min = arr[0];
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] < min) {
+				min = arr[i];
+			}
+		}
+		System.out.println(min + " " + max);
+	}
+
+}
