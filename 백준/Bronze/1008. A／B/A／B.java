c027b6e00a962a6fece78a8b9b029ace5b118ea2
JongHun Seo <162901825+jonghun0915@users.noreply.github.com>
2024-10-28 11:16:35 +0900

    [Bronze V] Title: A/B, Time: 216 ms, Memory: 17812 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		double a = scan.nextInt();
+		double b = scan.nextInt();
+
+		double sum = a / b;
+		System.out.println(sum);
+	}
+
+}
