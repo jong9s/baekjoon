b17bfcc8f0a8e2b625e7f9fd9609c8038095eb43
JongHun Seo <162901825+jonghun0915@users.noreply.github.com>
2024-10-29 09:47:10 +0900

    [Bronze II] Title: 공 바꾸기, Time: 208 ms, Memory: 18468 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int N = scan.nextInt();
+		int M = scan.nextInt();
+		int[] arr = new int[N];
+		int temp;
+		
+		
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = i + 1;
+		}
+		for (int j = 0; j < M; j++) {
+			int I = scan.nextInt();
+			int J = scan.nextInt();
+			
+			temp = arr[I-1];
+			arr[I-1] = arr[J-1];
+			arr[J-1] = temp;
+		}
+		for (int k = 0; k < arr.length; k++) {
+			System.out.print(arr[k] + " ");
+		}
+		
+		
+		
+		
+	}
+}
