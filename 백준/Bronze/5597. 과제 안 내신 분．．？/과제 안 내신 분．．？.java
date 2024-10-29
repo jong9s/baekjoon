34db5e04052e502dbf63a501bcd9b20d91cc2787
JongHun Seo <162901825+jonghun0915@users.noreply.github.com>
2024-10-29 10:51:51 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 172 ms, Memory: 17528 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int[] arr = new int[31];
+		
+		for (int i = 1; i < 29; i++) {
+			int n = scan.nextInt();
+			arr[n] = 1;
+		}
+		for (int i = 1; i < arr.length; i++) {
+			if(arr[i]!=1)
+				System.out.println(i);
+		}
+	}
+
+}
